22194700ed4004410c21351a93f466dac92d1ad0
Dan Klishch <danilklishch@gmail.com>
2025-10-29 14:21:33 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import asyncio
+import logging
+from pathlib import Path
+import shutil
+
+from src.install import Install
+from src.network import Network
+
+
+async def main() -> None:
+    working_dir = Path("/home/danklishch/code/ton/network2")
+    shutil.rmtree(working_dir, ignore_errors=True)
+    working_dir.mkdir(exist_ok=True)
+
+    install = Install(
+        Path("/home/danklishch/code/ton/src/build"),
+        Path("/home/danklishch/code/ton/src"),
+    )
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d][%(name)s] %(message)s",
+        datefmt="%Y-%m-%d %H-%M-%S",
+    )
+
+    async with Network(install, working_dir) as network:
+        dht = network.create_dht_node()
+
+        node = network.create_full_node()
+        node.make_initial_validator()
+        node.announce_to(dht)
+
+        await dht.run()
+        await node.run()
+
+        await network.wait_mc_block(seqno=1)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ src/install.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+import os
+from pathlib import Path
+import subprocess
+
+
+@dataclass
+class Install:
+    build_dir: Path
+    source_dir: Path
+
+    @property
+    def fift_exe(self):
+        return self.build_dir / "crypto/create-state"
+
+    @property
+    def fift_include_dirs(self):
+        return [
+            self.source_dir / "crypto/fift/lib",
+            self.source_dir / "crypto/smartcont",
+        ]
+
+    @property
+    def key_helper_exe(self):
+        return self.build_dir / "utils/generate-random-id"
+
+    @property
+    def validator_engine_exe(self):
+        return self.build_dir / "validator-engine/validator-engine"
+
+    @property
+    def dht_server_exe(self):
+        return self.build_dir / "dht-server/dht-server"
+
+    @property
+    def tonlibjson(self):
+        return self.build_dir / "tonlib/libtonlibjson.so"
+
+
+def run_fift(install: Install, code: str, working_dir: Path):
+    script_file = working_dir / "script.fif"
+    _ = script_file.write_text(code)
+
+    args = [install.fift_exe]
+    for include_dir in install.fift_include_dirs:
+        args += ["-I", include_dir]
+    args += ["-s", "script.fif"]
+
+    _ = subprocess.run(args, cwd=working_dir, check=True)
+
+    os.remove(script_file)
